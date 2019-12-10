8c39a7bcdd0cb66d7b4f6711098185996547c101
Robert Lemiesz <lemiesz@amazon.com>
2019-12-09 16:23:13 -0800

    Adding a README file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# large-file-generator
-Generates a large text file 
+# How to run
+
+1.) Make sure you install node one your machine
+
+```
+brew install nodejs
+```
+
+2.) Set the number of iterations you want in generate.js the default generates a 5gb file
+
+3.) `node generate.js`
--- generate.js
+++ generate.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 
+const ITERATIONS = 100000; // Default generates a 5gb text file
+
 function generate() {
   const writeStream = fs.createWriteStream("./out.txt");
   fs.readFile("./beemovie.txt", function(err, data) {
     if (err) throw err;
-    for (let i = 0; i < 100000; i++) {
+    for (let i = 0; i < ITERATIONS; i++) {
       writeStream.write(`${data} \n`);
     }
   });
